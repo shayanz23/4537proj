eb1e318a5935cba5fb49063d3188e3fac3deef6d
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2023-11-20 21:20:12 -0800

    removed attempt at improving body

--- project_client/src/components/body.tsx
+++ project_client/src/components/body.tsx
@@ -7,23 +7,20 @@ import Admin from "../pages/Admin";
 import NotFound from "../pages/NotFound";
 import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
-import "./Body.css";
 
 const Body = () => {
   return (
-    <div className="body">
-      <Routes>
-        {/* The Switch decides which component to show based on the current URL.*/}
-        <Route path="/" element={<Index />}></Route>
-        <Route path="/ask" element={<Ask />}></Route>
-        <Route path="/answer" element={<Answer />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/admin" element={<Admin />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <Routes>
+      {/* The Switch decides which component to show based on the current URL.*/}
+      <Route path="/" element={<Index />}></Route>
+      <Route path="/ask" element={<Ask />}></Route>
+      <Route path="/answer" element={<Answer />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/admin" element={<Admin />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
 
