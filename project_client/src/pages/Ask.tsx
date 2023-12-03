a134718377c8b4bc3a79b6eff98462976d377499
kenarrr <itskenar@gmail.com>
2023-12-03 11:11:33 -0800

    Modified code so it shows whole conversation, and UI changes

--- project_client/src/pages/Ask.tsx
+++ project_client/src/pages/Ask.tsx
@@ -3,27 +3,47 @@ import { Navigate } from "react-router-dom";
 import { useState } from "react";
 import "./container.css";
 
+type Message = {
+  text: string;
+  sender: 'user' | 'ai';
+};
+
 function Ask() {
-  const [question, setQuestion] = useState<string>("");
-  const [answer, setAnswer] = useState<string>("");
+  const [question, setQuestion] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const addMessage = (text: string, sender: 'user' | 'ai') => {
+    const newMessage = { text, sender };
+    setMessages([...messages, newMessage]);
+  };
+
   const QuestionEventHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const userMessage: Message = { text: question, sender: 'user' };
+    addMessage(question, 'user');
+  
     const response = await fetch(
       "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill",
       {
-        headers: { Authorization: "Bearer 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" },
-			method: "POST",
-			body: JSON.stringify(question),
-
+        headers: {
+          Authorization: "Bearer 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",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({ inputs: question }),
       }
     );
     const result = await response.json();
-    console.log(result);
+  
     if (result && result.generated_text) {
-      setAnswer(result.generated_text);
+      const aiMessage: Message = { text: result.generated_text, sender: 'ai' };
+      addMessage(result.generated_text, 'ai');
+      setMessages([...messages, userMessage, aiMessage]); // Append both user's and AI's messages
     } else {
-      setAnswer("No response from the AI.");
+      addMessage("No response from the AI.", 'ai');
+      setMessages([...messages, userMessage]); // Append only user's message if no AI response
     }
+    setQuestion(""); // Clear input after submitting
   };
   
 
@@ -31,38 +51,30 @@ function Ask() {
   if (cookies.get("user") !== null && cookies.get("user") !== undefined) {
     return (
       <div className="container">
-        <h2>Ask your Question! We will give UNHINGED advice!</h2>
+        <div className="conversation">
+          {messages.map((message, index) => (
+            <div key={index} className={`message ${message.sender}`}>
+              {message.text}
+            </div>
+          ))}
+        </div>
         <form onSubmit={QuestionEventHandler}>
           <input
             type="text"
             name="question"
             id="question"
-            placeholder="Question"
+            placeholder="Type a question..."
             value={question}
             onChange={(e) => setQuestion(e.target.value)}
           />
-          <p>{"\n"}</p>
-          <button type="submit" className="btn btn-primary btn-block btn-large">
-            Submit Question
-          </button>
-          <p>{"\n"}</p>
+          <button type="submit">Submit</button>
         </form>
-        <p>{"\n"}</p>
-        <textarea
-          name="text"
-          id="answer_area_1"
-          className="form-control"
-          cols={30}
-          rows={25}
-          title="Answer 1"
-          value={answer}
-          readOnly
-        ></textarea>
       </div>
     );
   } else {
     return <Navigate to="/login" />;
   }
 }
+
 
 export default Ask;
