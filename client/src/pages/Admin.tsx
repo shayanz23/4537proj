a2edc85e1816cc77ffacf9bc0dc40160dd34f064
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2023-12-06 22:27:24 -0800

    minor change because life is BS

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -164,7 +164,7 @@ export default function Dashboard() {
 
   return (
     <div className="container">
-      <h1 style={{ width: "100%", marginTop: "10px" }}>{strings.aDash}</h1>
+      <h1 style={{ width: "100%", marginTop: "10px" }}>{strings ? strings.aDash: 'Loading...'}</h1>
       <table style={{ width: "100%", marginTop: "10px" }}>
         <thead>
           <tr>
